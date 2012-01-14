ad4c65905b97b9ff1977710c221e2fc7aeb8b6fc
SancezZ <darkspicy@gmx.de>
2012-01-14 22:55:16 +0100

    first push

---
+++ asdfghjl/src/blaaa/blubb/AsdfghjlActivity.java
@@ -0,0 +1,13 @@
+package blaaa.blubb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AsdfghjlActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
